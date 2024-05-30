849db77613e2313c912782cf99cde3d245598410
banira4649 <banira.game@outlook.jp>
2024-05-30 17:22:44 +0900

    Initial commit

--- src/Main.php
+++ src/Main.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace banira4649\WorldHelper;
 
+use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use banira4649\WorldHelper\commands\WorldCommand;
 
-class Main extends PluginBase implements \pocketmine\event\Listener{
+class Main extends PluginBase implements Listener{
 
     public function onEnable(): void{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
--- src/commands/WorldCommand.php
+++ src/commands/WorldCommand.php
@@ -6,58 +6,61 @@ namespace banira4649\WorldHelper\commands;
 
 use pocketmine\command\{Command, CommandSender};
 use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\world\WorldManager;
 
 class WorldCommand extends Command{
 
-    public function __construct(string $name, $main){
+    private WorldManager $worldManager;
+
+    public function __construct(string $name, PluginBase $main){
         $this->setPermission("pocketmine.group.operator");
         parent::__construct($name, "ワールドに関する操作を行います");
-        $this->main = $main;
-        $this->server = $main->getServer();
         $this->worldManager = $main->getServer()->getWorldManager();
     }
 
-    public function execute(CommandSender $sender, string $label, array $args){
-        if(!$this->testPermission($sender)) return;
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool{
+        if(!$this->testPermission($sender)) return false;
         if($args !== []){
             switch($args[0]){
                 case "load":
-                if(isset($args[1])){
-                    $result = $this->worldManager->loadWorld($args[1]);
-                    $result ? $sender->sendMessage("§aWorld loaded successfully.") : $sender->sendMessage("§cFailed to load the world.");
-                }
-                return true;
+                    if(isset($args[1])){
+                        $result = $this->worldManager->loadWorld($args[1], true);
+                        $result ? $sender->sendMessage("§aWorld loaded successfully.") : $sender->sendMessage("§cFailed to load the world.");
+                    }
+                    return true;
 
                 case "unload":
-                if(isset($args[1])){
-                    $result = $this->worldManager->unloadWorld($args[1]);
-                    $result ? $sender->sendMessage("§aWorld unloaded successfully.") : $sender->sendMessage("§cFailed to unload the world.");
-                }
-                return true;
+                    if(isset($args[1])){
+                        $result = $this->worldManager->unloadWorld($args[1]);
+                        $result ? $sender->sendMessage("§aWorld unloaded successfully.") : $sender->sendMessage("§cFailed to unload the world.");
+                    }
+                    return true;
 
                 case "tp":
-                if($sender instanceof Player){
-                    if(isset($args[1])){
-                        $this->worldManager->loadWorld($args[1]);
-                        $world = $this->worldManager->getWorldByName($args[1]);
-                        $world !== null ? $sender->sendMessage("§aYou teleported successfully.") : $sender->sendMessage("§cWorld not found.");
-                        $sender->teleport($world?->getSafeSpawn() ?? $sender->getLocation());
+                    if($sender instanceof Player){
+                        if(isset($args[1])){
+                            $this->worldManager->loadWorld($args[1], true);
+                            $world = $this->worldManager->getWorldByName($args[1]);
+                            $world !== null ? $sender->sendMessage("§aYou teleported successfully.") : $sender->sendMessage("§cWorld not found.");
+                            $sender->teleport($world?->getSafeSpawn() ?? $sender->getLocation());
+                        }
                     }
-                }
-                return true;
+                    return true;
 
                 case "list":
-                $worlds = $this->worldManager->getWorlds();
-                $list = [];
-                foreach($worlds as $world){
-                    $name = $world->getDisplayName();
-                    array_push($list, $name);
-                }
-                $list = implode(", ", $list);
-                $sender->sendMessage("§aロードされているワールド: "."\n"."§b".$list);
-                return true;
+                    $worlds = $this->worldManager->getWorlds();
+                    $list = [];
+                    foreach($worlds as $world){
+                        $name = $world->getDisplayName();
+                        $list[] = $name;
+                    }
+                    $list = implode(", ", $list);
+                    $sender->sendMessage("§aロードされているワールド: "."\n"."§b".$list);
+                    return true;
             }
         }
+        return false;
     }
 
 }
